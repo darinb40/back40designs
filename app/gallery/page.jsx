8681b52a70a01253061e1d6c63d34a1fa3649c6e
darinb40 <darin@back40designco.com>
2026-03-27 19:55:08 -0500

    Update page.jsx

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -130,7 +130,7 @@ export default function GalleryPage() {
             </div>
           </div>
 
-          <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4>
             {gallery.map((item, index) => (
               <button
                 key={`${item.title}-${index}`}
